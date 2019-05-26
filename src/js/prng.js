2415a679d4bcfad1fcdc3b1fcbd3ccfb51224d9e
Ancho <2279469+anchowake@users.noreply.github.com>
2019-05-26 00:33:21 -0500

    Polishing the implementation

--- src/js/prng.js
+++ src/js/prng.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const min = 0;
     const max = 1000000;
     let randomNumber = 0;
-    let seed = 0;
     
     if (n >= max || n <= min) {
       return `The input number must be between: ${min} and ${max}`;
@@ -17,8 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
     
     function MSWS(seed) {
       let x = seed * seed; // Middle Squares
-      let w = seed * Date.now(); // Date.now() to add more salt
-      let s = 362437;
+      const w = seed * Date.now(); // Date.now() to add more salt
+      const s = 362437;
       
       x += (w + s); // Weyl Sequence 
       x = String(x);
@@ -26,12 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
       return x.substring(x.length / 2 - 3, x.length / 2 + 3); // Retrieves the middle values of the sum string
     }
     
-    const rngInt = Array.from(new Array(n), () => flip() === true ? 1 : 0)
+    const rngInt = Array.from(new Array(n), () => flip() ? 1 : 0)
     .reduce((bitSum, bit) => {
       return bitSum + bit;
     });
     
-    seed = MSWS(rngInt);
+    let seed = MSWS(rngInt);
     randomNumber = Math.floor(seed / max * n);
     
     return randomNumber;
